c7be2cae462b12b1a70b4f5f7ab0eb1dc84ce156
liub0v <kurets2001@gmail.com>
2021-06-24 17:04:34 +0300

    lesson 8. splitting code

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+return(
+    <header className="header">
+        <img
+            src="https://static.vecteezy.com/system/resources/previews/001/192/261/non_2x/circle-abstract-logo-png.png"/>
+    </header>
+);
+}
+
+export default Header;
---
+++ src/components/Profile.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const Profile = () => {
+return(
+    <div className="content">
+        <div>
+            Backgroung
+        </div>
+        <div> profile info (avatar + description)</div>
+        <div>
+            My posts
+            <div>
+                New post
+            </div>
+            <div>
+                Post 1
+            </div>
+            <div>
+                Post 2
+            </div>
+        </div>
+    </div>
+);
+}
+export default Profile;
